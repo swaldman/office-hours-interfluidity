67bf920423f2d02ac452049bd73b05369e5ca54d
Steve Waldman <swaldman@mchange.com>
2023-10-21 12:02:10 -0400

    Add some comments, clean things up.

--- src/com/interfluidity/officehours/hedgedoc/core.scala
+++ src/com/interfluidity/officehours/hedgedoc/core.scala
@@ -2,6 +2,13 @@ package com.interfluidity.officehours.hedgedoc
 
 import java.net.URLEncoder
 
+// to play with the logic that this implements interactively and debug,
+//   see https://github.com/swaldman/scalascripts-misc/blob/main/bin/hedgedoc-new-note
+//
+// we rely upon notes being created with `defaultPermission` `freely`, as there is
+// no API for setting permissions.
+//   see "Update 2023-10-20", https://tech.interfluidity.com/2023/08/23/getting-started-with-hedgedoc/index.html#update-2023-10-20
+
 def newNote( hedgedocUrl : String, noteOwnerEmail : String, noteOwnerPassword : String, noteId : String, initialMarkdown : String ) : Either[String,Unit] =
   val stdurl =
     if hedgedocUrl.endsWith("/") then hedgedocUrl.init else hedgedocUrl
@@ -14,7 +21,12 @@ def newNote( hedgedocUrl : String, noteOwnerEmail : String, noteOwnerPassword : 
       check=false
     )
   val createResponse =
-    session.post(s"${stdurl}/new/${URLEncoder.encode(noteId)}", data=initialMarkdown, maxRedirects=0, check=false)
+    session.post(
+      s"${stdurl}/new/${URLEncoder.encode(noteId)}",
+      data=initialMarkdown,
+      maxRedirects=0,
+      check=false
+    )
   createResponse.statusCode match
     case 302 => Right( () ) // this is what we expect if it worked, it tries to redirect
     case 409 => Left(s"It appears that note '${noteId}' already exists.")
